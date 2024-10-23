3f6120a7dba68f5ff95ceefdd8d1f8e26abe44b0
John Albert T. Presentacion <johnalbertpresentacion@gmail.com>
2024-10-23 12:11:22 +0800

    add sql properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
+CREATE TABLE IF NOT EXISTS post (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    content TEXT
+);
+
 INSERT INTO post (title, content) VALUES ('First Post', 'This is the content of the first post.');
 INSERT INTO post (title, content) VALUES ('Second Post', 'This is the content of the second post.');
